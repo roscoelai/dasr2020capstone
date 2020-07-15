a9d30b7201008bf65c1a0200a862a10bc6d238f5
Roscoe Lai <LAIZYR@sics.a-star.edu.sg>
2020-07-15 16:01:50 +0800

    Add clinics and assign stations

--- src/read_kml.R
+++ src/read_kml.R
@@ -14,10 +14,13 @@ library(magrittr)
 # 6. Import climate station positions
 # 7. Plot planning areas, polygons of cases, points of cases, climate stations
 
-# TODO:
-# - Assign climate stations to planning areas
 # - Assign populations to planning areas
 # - Assign number of clinics to planning areas
+# - Assign planning areas to climate stations
+
+# TODO:
+# - Assign population age groups to planning areas
+# - Area? Sea areas are problematic...
 
 # Get dengue cases ----
 
@@ -44,6 +47,28 @@ dengue_points <- dengue_polys %>%
   .[rep(1:nrow(.), .$n),] %>% 
   dplyr::select(-Name, -n)
 
+
+populations <- 
+  paste0("../data/data_gov_2/",
+         "resident-population-by-planning-area-subzone-age-group-and-sex-2015/",
+         "resident-population-by-planning-area-age-group-and-sex.csv") %>% 
+  readr::read_csv() %>% 
+  dplyr::rename(sex = level_1,
+                age_group = level_2,
+                plan_area = level_3,
+                pop = value) %>% 
+  dplyr::filter(sex == "Total" & 
+                  age_group == "Total" & 
+                  plan_area != "Total") %>% 
+  dplyr::mutate(plan_area = gsub("- Total", "", plan_area)) %>% 
+  dplyr::select(-year, -sex, -age_group)
+
+chas_points <- "../data/data_gov_2/chas-clinics/chas-clinics-kml.kml" %>% 
+  sf::st_read() %>% 
+  dplyr::select(-Name, -Description) %>% 
+  sf::st_zm()
+
+
 planning_areas <- 
   paste0("../data/data_gov_2/master-plan-2019-planning-area-boundary-no-sea/",
          "planning-boundary-area.kml") %>% 
@@ -54,49 +79,82 @@ planning_areas <-
   sf::st_as_sf() %>% 
   sf::st_zm()
 
-# Points within area ----
-
-# Check Coordinate Reference Systems
-# sf::st_crs(dengue_points)
-# sf::st_crs(planning_areas)
-
-planning_areas <- sf::st_intersects(dengue_points, planning_areas) %>% 
-  tibble::as_tibble() %>% 
-  dplyr::rename(Name = col.id) %>% 
-  dplyr::mutate(Name = paste0("kml_", Name)) %>% 
-  dplyr::group_by(Name) %>% 
-  dplyr::count() %>% 
-  dplyr::inner_join(planning_areas, by = "Name") %>% 
-  dplyr::mutate(label = htmltools::HTML(paste0(plan_area, ":<br/>", n))) %>% 
-  sf::st_as_sf()
-
 climate_stations <- readr::read_csv("../data/Station_Records.csv") %>% 
   dplyr::filter(Station %in% c("Ang Mo Kio",
                                "Changi",
                                "Pasir Panjang",
                                "Tai Seng")) %>% 
   dplyr::select(Station, matches("Lat|Long")) %>% 
-  sf::st_as_sf(coords = c("Long. (E)", "Lat.(N)"))
+  sf::st_as_sf(coords = c("Long. (E)", "Lat.(N)")) %>% 
+  sf::`st_crs<-`("wgs84")
+
+planning_areas$stn <- planning_areas %>% 
+  sf::st_centroid() %>% 
+  sf::st_distance(climate_stations) %>% 
+  apply(1, FUN = which.min) %>% 
+  climate_stations$Station[.]
+
+# planning_areas$area <- sf::st_area(planning_areas)
+
+# Points within area ----
+
+# Check Coordinate Reference Systems
+# sf::st_crs(dengue_points)
+# sf::st_crs(planning_areas)
+
+ncases_in_areas <- sf::st_intersects(dengue_points, planning_areas) %>% 
+  tibble::as_tibble() %>% 
+  dplyr::rename(Name = col.id) %>% 
+  dplyr::mutate(Name = paste0("kml_", Name)) %>% 
+  dplyr::group_by(Name) %>% 
+  dplyr::count(name = "ncases")
+
+nclinics_in_areas <- sf::st_intersects(chas_points, planning_areas) %>% 
+  tibble::as_tibble() %>% 
+  dplyr::rename(Name = col.id) %>% 
+  dplyr::mutate(Name = paste0("kml_", Name)) %>% 
+  dplyr::group_by(Name) %>% 
+  dplyr::count(name = "nclinics")
+
+joined_table <- ncases_in_areas %>% 
+  dplyr::inner_join(nclinics_in_areas, by = "Name") %>% 
+  dplyr::inner_join(planning_areas, by = "Name") %>% 
+  dplyr::inner_join(populations, by = "plan_area") %>% 
+  dplyr::mutate(label = htmltools::HTML(paste0(plan_area,
+                                               "<br/>Cases: ",
+                                               ncases,
+                                               "<br/>CHAS Clinics: ",
+                                               nclinics,
+                                               "<br/>Population: ",
+                                               pop,
+                                               # "<br/>Area: ",
+                                               # area,
+                                               "<br/>Climate station: ",
+                                               stn))) %>% 
+  sf::st_as_sf()
 
 # Visualize ----
-
-cases_pal <- leaflet::colorNumeric("Reds", dengue_polys$n)
-
 set.seed(336483)
 
 area_pal <- colors() %>% 
   .[grep("gr(a|e)y", ., invert = T)] %>% 
-  sample(nrow(planning_areas)) %>% 
+  # sample(nrow(planning_areas)) %>% 
+  sample(nrow(climate_stations)) %>% 
   leaflet::colorFactor(NULL)
+
+# area_cases_pal <- leaflet::colorNumeric("Reds", joined_table$ncases)
+cases_pal <- leaflet::colorNumeric("Reds", dengue_polys$n)
 
 leaflet::leaflet(height = 700, width = "100%") %>%
   leaflet::addTiles() %>%
-  leaflet::addPolygons(data = planning_areas,
+  leaflet::addPolygons(data = joined_table,
                        weight = 1,
                        opacity = 1,
                        smoothFactor = 0.5,
                        fillOpacity = 0.5,
-                       fillColor = ~area_pal(plan_area),
+                       # fillColor = ~area_pal(plan_area),
+                       fillColor = ~area_pal(stn),
+                       # fillColor = ~area_cases_pal(ncases),
                        label = ~label,
                        popup = ~label) %>%
   leaflet::addPolygons(data = dengue_polys,
@@ -115,16 +173,35 @@ leaflet::leaflet(height = 700, width = "100%") %>%
                       label = ~Station) %>%
   {.}
 
+joined_table %>% 
+  dplyr::group_by(stn) %>% 
+  dplyr::summarize(ncases = sum(ncases))
+
 
 
 # Plot Dengue Clusters (From NEA/Data.gov.sg) ----
-quick_leaf <- function(paths) {
+quick_polys <- function(paths) {
   paths %>% 
     sf::st_read() %>% 
+    sf::st_zm() %>% 
     leaflet::leaflet(width = "100%") %>% 
     leaflet::addTiles() %>% 
     leaflet::addPolygons(popup = ~Description,
                          weight = 1)
 }
 
-quick_leaf("../data/data_gov_1/dengue-clusters/dengue-clusters-kml.kml")
+quick_points <- function(paths) {
+  paths %>% 
+    sf::st_read() %>% 
+    sf::st_zm() %>% 
+    leaflet::leaflet(width = "100%") %>%
+    leaflet::addTiles() %>%
+    leaflet::addCircleMarkers(radius = 5,
+                              color = "red",
+                              fillOpacity = 0.5,
+                              popup = ~Description,
+                              clusterOptions = leaflet::markerClusterOptions())
+}
+
+quick_polys("../data/data_gov_1/dengue-clusters/dengue-clusters-kml.kml")
+quick_points("../data/data_gov_2/chas-clinics/chas-clinics-kml.kml")
